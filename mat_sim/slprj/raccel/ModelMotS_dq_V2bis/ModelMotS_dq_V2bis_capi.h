fa28c103d304d5884cdac92a294228e8797a461b
sagarverma <sagar15056@iiitd.ac.in>
2019-06-21 18:55:03 +0200

    .gitignore is now working

--- mat_sim/slprj/raccel/ModelMotS_dq_V2bis/ModelMotS_dq_V2bis_capi.h
+++
@@ -1,6 +0,0 @@
-#include "__cf_ModelMotS_dq_V2bis.h"
-#ifndef RTW_HEADER_ModelMotS_dq_V2bis_capi_h
-#define RTW_HEADER_ModelMotS_dq_V2bis_capi_h
-#include "ModelMotS_dq_V2bis.h"
-extern void ModelMotS_dq_V2bis_InitializeDataMapInfo ( void ) ;
-#endif
--- mat_sim/slprj/raccel/ModelMotS_dq_V2bis/__cf_ModelMotS_dq_V2bis.h
+++
@@ -1,3 +0,0 @@
-#ifndef CF_ModelMotS_dq_V2bis_H__
-#define CF_ModelMotS_dq_V2bis_H__
-#endif
